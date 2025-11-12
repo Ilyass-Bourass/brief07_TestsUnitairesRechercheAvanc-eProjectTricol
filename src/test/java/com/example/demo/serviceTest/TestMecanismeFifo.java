fb9878f57d832f1648d356c8df90d5e5aa916f10
ilyass bourass <ilyass.b.1992@gmail.com>
2025-11-12 20:49:58 +0100

    BRIEF-7 Sortie nécessitant la consommation de plusieurs lots

--- src/test/java/com/example/demo/serviceTest/TestMecanismeFifo.java
+++ src/test/java/com/example/demo/serviceTest/TestMecanismeFifo.java
@@ -13,6 +13,7 @@ import com.example.demo.repository.StockRepository;
 import com.example.demo.service.BonSortieService;
 import com.example.demo.service.impl.BonSortieImpl;
 import com.example.demo.service.impl.StockServiceImpl;
+import jdk.jfr.Name;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -69,13 +70,13 @@ public class TestMecanismeFifo {
      void senario_un_consomation_partielle_un_seul_lot(){
 
          Stock lot =Stock.builder().id(1L).quantite(100).produit(produit).build();
-         ResponseStockDTO responseStockDTO =ResponseStockDTO.builder().id(1L).produitId(1L).build();
+         ResponseStockDTO responseStockByProduitDTO =ResponseStockDTO.builder().id(1L).produitId(1L).build();
 
          when(produitRepository.findById(1L)).thenReturn(Optional.of(produit));
          when(stockRepository.findById(1L)).thenReturn(Optional.of(lot));
          when(stockRepository.save(any(Stock.class))).thenReturn(lot);
          when(bonSortieRepository.findById(1L)).thenReturn(Optional.of(bonSortie));
-         when(stockService.getStocksByProduit(1L)).thenReturn(List.of(responseStockDTO));
+         when(stockService.getStocksByProduit(1L)).thenReturn(List.of(responseStockByProduitDTO));
 
          MouvementStock mouvementStock = new MouvementStock();
          when(stockVersMouvementStockMapper.stockVersMouvementStock(any(Stock.class)))
@@ -92,6 +93,52 @@ public class TestMecanismeFifo {
          verify(stockRepository, times(1)).save(lot);
          verify(produitRepository, times(1)).save(produit);
          verify(bonSortieRepository, times(1)).save(bonSortie);
+     }
+
+     @Test
+     void senarion_deux_consomation_plusieurs_lot(){
+
+        Stock lot01=Stock.builder().id(1L).quantite(10).produit(produit).build();
+        Stock lot02=Stock.builder().id(2L).quantite(40).produit(produit).build();
+
+        ResponseStockDTO responseStockByProduitDTO01 =ResponseStockDTO.builder().id(1L).produitId(1L).build();
+        ResponseStockDTO responseStockByProduitDTO02 =ResponseStockDTO.builder().id(2L).produitId(1L).build();
+
+         when(produitRepository.findById(1L)).thenReturn(Optional.of(produit));
+         when(stockRepository.findById(1L)).thenReturn(Optional.of(lot01));
+         when(stockRepository.findById(2L)).thenReturn(Optional.of(lot02));
+         when(stockRepository.save(lot01)).thenReturn(lot01);
+         when(stockRepository.save(lot02)).thenReturn(lot02);
+         when(bonSortieRepository.findById(1L)).thenReturn(Optional.of(bonSortie));
+         when(stockService.getStocksByProduit(1L)).thenReturn(List.of(responseStockByProduitDTO01,responseStockByProduitDTO02));
+
+         MouvementStock mouvementStock01 = new MouvementStock();
+         MouvementStock mouvementStock02 = new MouvementStock();
+         when(stockVersMouvementStockMapper.stockVersMouvementStock(lot01))
+                 .thenReturn(mouvementStock01);
+         when(stockVersMouvementStockMapper.stockVersMouvementStock(lot02))
+                 .thenReturn(mouvementStock02);
+
+         String result= bonSortieService.ValiderBonSortie(1L);
+         assertEquals(0,lot01.getQuantite(),"le stock 01 vide");
+         assertEquals(20,lot02.getQuantite(),"le stock 02 doit dépasser de 40 à 30");
+         assertEquals(StatutBonSortie.VALIDE,bonSortie.getStatutBonSortie());
+         assertEquals(TypeMouvement.SORTIE,mouvementStock01.getTypeMouvement());
+         assertEquals(TypeMouvement.SORTIE,mouvementStock02.getTypeMouvement());
+         assertEquals(70,produit.getStockActuel(),"le stock dépasser de 100 à 70");
+         assertTrue(result.contains("effuctué avec succée"));
+
+         verify(bonSortieRepository, times(1)).save(bonSortie);
+         verify(stockRepository, times(2)).save(any(Stock.class));
+         verify(mouvementStockRepository, times(2)).save(any(MouvementStock.class));
+         verify(produitRepository, times(2)).save(any(Produit.class));
+     }
+    /*
+     @Test
+     void senario_03_Sortie_avec_stock_insuffisant(){
+        Stock stock01=Stock.builder().id(1L).produit(produit).quantite(40).build();
 
      }
+
+     */
 }
